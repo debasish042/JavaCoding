62eac58bdd3d08a160dd32b6b8c8eaced2a557bd
Debasish <sahudebasish012@gmail.com>
2026-03-18 16:48:30 +0530

    java coding practice day 4

---
+++ src/FindFrequencyOfEachCharacter.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Map;
+
+public class FindFrequencyOfEachCharacter {
+    public static void main(String[] args) {
+        String str = "Debasish";
+
+        Map<Character,Integer> map = new LinkedHashMap<>();
+
+        for(char c : str.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        System.out.println(map);
+    }
+}
